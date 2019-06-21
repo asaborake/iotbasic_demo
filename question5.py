85109ff838e2a3839d618bf670425ba88a0eee7e
Eita Hisamitsu <hisamitsu@jissen-cl.co.jp>
2019-06-21 12:02:27 +0900

    ジャンケンゲーム完成

--- question5.py
+++ question5.py
@@ -1,35 +1,70 @@
 # -*- coding: utf-8 -*-
-import cv2
-import sys
 import picamera
 import picamera.array
 
 import collections
+import random
 import recognition as recog
+
 from sklearn.externals import joblib
 
-# 学習済みモデルの読み込み
 clf = joblib.load('recognition.pk1')
+hands = ['グー','チョキ','パー']
+human_hands = []
+jyanken_count = 0
+cpu_win = 0
+human_win = 0
 
-jyanken_hands = []
-with picamera.PiCamera() as camera:
-    with picamera.array.PiRGBArray(camera) as stream:
-        camera.resolution = (320, 240)
-        camera.framerate = 15
-        camera.awb_mode = 'fluorescent'
+def handRecognition():
+    with picamera.PiCamera() as camera:
+        with picamera.array.PiRGBArray(camera) as stream:
+            camera.resolution = (320, 240)
+            camera.framerate = 15
+            camera.awb_mode = 'fluorescent'
 
-        while len(jyanken_hands) < 6:
-            camera.capture(stream, 'bgr', use_video_port=True)
-            hand_vector = recog.getImageVector(stream.array)
-            result = clf.predict(hand_vector)
-            jyanken_hands.append(result[0])
-            stream.seek(0)
-            stream.truncate()
+            while len(human_hands) < 6:
+                camera.capture(stream, 'bgr', use_video_port=True)
+                hand_vector = recog.getImageVector(stream.array)
+                result = clf.predict(hand_vector)
+                human_hands.append(result[0])
+                stream.seek(0)
+                stream.truncate()
 
-        print(jyanken_hands)
+            c = collections.Counter(human_hands)
+            (h, num) = c.most_common()[0]
 
-        #c = collections.Counter(jyanken_hands)
-        #(jyanken_hand, num) = c.most_common()[0]
-        #print(jyanken_hand)
+    return h
 
-        cv2.destroyAllWindows()
+while jyanken_count < 10:
+    print('ジャンケンポン')
+    human_hand = handRecognition()
+    machine_hand = random.choice([0,1,2])
+    print('あなたは' + hands[human_hand] + 'で、私は' + hands[machine_hand] + 'だから')
+
+    if(human_hand == machine_hand):
+        print('あいこで、もう１回')
+    elif(human_hand > machine_hand):
+        if(human_hand - machine_hand == 1):
+            print('私の勝ち')
+            cpu_win += 1
+        else:
+            print('私の負け')
+            human_win += 1
+        jyanken_count += 1
+    elif(human_hand < machine_hand):
+        if(human_hand - machine_hand >= -1):
+            print('私の負け')
+            human_win += 1
+        else:
+            print('私の勝ち')
+            cpu_win += 1
+        jyanken_count += 1
+
+if(cpu_win == human_win):
+    winner = '引き分けです。'
+elif(cpu_win > human_win):
+    winner = '私の勝ちです。'
+elif(cpu_win < human_win):
+    winner = 'あなたの勝ちです。'
+
+print('あなたの' + str(human_win) + '勝' + str(cpu_win) + '敗で' + winner)
