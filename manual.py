f625577c8e46c16ae73c2a57b651aed142e3431e
Eita Hisamitsu <hisamitsu@jissen-cl.co.jp>
2019-06-21 14:14:47 +0900

    数字読み込んで足し上げる

--- manual.py
+++ manual.py
@@ -2,23 +2,42 @@
 import picamera
 import picamera.array
 import cv2
+import time
+import collections
 import recognition_num as recog
+
+recog_numbers = []
+numbers = []
+repeat = 1
+total = 0
 
 with picamera.PiCamera() as camera:
     with picamera.array.PiRGBArray(camera) as stream:
         camera.resolution = (320, 240)
+        while repeat < 3:
+            if repeat == 1:
+                print('一つ目の数字をカメラに見せて')
+            else:
+                print('二つ目の数字をカメラに見せて')
+                recog_numbers = []
+                time.sleep(2)
 
-        while True:
-            camera.capture(stream, 'bgr', use_video_port=True)
-            gray = cv2.cvtColor(stream.array, cv2.COLOR_BGR2GRAY)
-            (ret, binary) = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-            number = recog.recog(binary)
-            print(number)
-
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-
-            stream.seek(0)
-            stream.truncate()
-
-        cv2.destroyAllWindows()
+            while len(recog_numbers) < 6:
+                camera.capture(stream, 'bgr', use_video_port=True)
+                gray = cv2.cvtColor(stream.array, cv2.COLOR_BGR2GRAY)
+                (ret, binary) = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+                number = recog.recog(binary)
+                recog_numbers.append(number[0])
+                stream.seek(0)
+                stream.truncate()
+        
+            #print(recog_numbers)
+            c = collections.Counter(recog_numbers)
+            (h, num) = c.most_common()[0]
+            numbers.append(h)
+            repeat += 1
+        
+        for i in numbers:
+            total += i
+        
+        print(total)
